d348fa91ed51b6ae3d655a1b0f01817f0abbae46
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-04-29 01:33:53 +0400

    Added More Info to Mem file

--- Version 1.0/JARVIS/mem.py
+++ Version 1.0/JARVIS/mem.py
@@ -39,6 +39,20 @@ def more_mem_info():
     
     return Panel(info_panel, title = "[b] MORE INFO ON MEMORY", box = box.SQUARE) 
 
+def more_info():
+    processor = platform.processor()
+    machine_type = platform.machine()
+    architecture = platform.architecture()[0]
+    platform_info = platform.platform()
+    os = platform.system()
+    
+    #print(processor + " || " + machine_type + " || " + architecture + " || " + platform_info + " || " + os)
+    # Intel64 Family 6 Model 126 Stepping 5, GenuineIntel || AMD64 || 64bit || Windows-10-10.0.19043-SP0 || Windows
+    
+    info_panel = Panel(f"[b]Processor : {processor}\nAMD version : {machine_type}\nArchitecture : {architecture}\nPlatform Info : {platform_info}\nOS : {os}", title = f"[b] {platform_info}", title_align = "left", box = box.SQUARE)
+    
+    return info_panel
+
 more_mem_info()
 
     
